ab9261d3696bc8f82822e729b16f2b94661cca68
st084331 <st084331@student.spbu.ru>
2023-05-30 03:21:33 +0300

    coarse-gained hash set IExam implementation

---
+++ IExamSystem/Implementation/CoarseGrainedHashSetImplementation.cs
@@ -0,0 +1,23 @@
+﻿namespace IExamSystem.Implementation;
+
+using IExamSystem.Collections;
+
+public class CoarseGrainedExamSystem : Interface.IExamSystem
+{
+    private readonly CoarseGrainedHashSet<Tuple<long, long>> _store;
+
+    public CoarseGrainedExamSystem()
+    {
+        _store = new CoarseGrainedHashSet<Tuple<long, long>>(20);
+    }
+
+    public CoarseGrainedExamSystem(int capacity)
+    {
+        _store = new CoarseGrainedHashSet<Tuple<long, long>>(capacity);
+    }
+
+    public int Count => _store.Count();
+    public void Add(long studentId, long courseId) => _store.Add(new(studentId, courseId));
+    public bool Contains(long studentId, long courseId) => _store.Contains(new(studentId, courseId));
+    public void Remove(long studentId, long courseId) => _store.Remove(new(studentId, courseId));
+}
